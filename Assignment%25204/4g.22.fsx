90fd4956537d1e17df8786a5acf05229a08a3c70
Megahest <jonas.horstmann1@gmail.com>
2016-10-05 16:51:05 +0200

    Final update

--- Assignment%25204/4g.22.fsx
+++ Assignment%25204/4g.22.fsx
@@ -8,29 +8,30 @@ printfn "             OPGAVE 4g.2"
 ///alle gentagne elementer, samtidig med at første forekomst bevares
 ///</summary>
 ///<params name="xs">
-///'a list 
+///'a list
 ///</params>
 ///<returns>
 ///En liste hvor kun den første forekomst af et element bevares
 ///</returns>
 ///<remarks>
-///Bruger List.foldBack og List.exists
+///Bruger List.foldBack og List.exists.
+///Definerer en funktion f h t først som den så kalder på sit input 'bagfra'
 ///</remarks>
-let rec rev i =
+let rec rev i =                 //Funktion til at vende lister om
     match i with
     |[] -> []
     |[u] -> [u]
     |p::q -> rev q @ [p]
 let rec removeDuplicates (xs : 'a list) =
-    let f h t =
+    let f h t =                 //Definerer funktionen som 'tjekker' og fjerner
         match t with
         | [] -> [h]
         | _ ->
             if List.exists(fun x -> x = h) t = false then h :: t
             else t
-    xs
+    xs                          //Tager inputtet
     |> rev
-    |> fun x -> List.foldBack f x []
+    |> fun x -> List.foldBack f x [] //Kalder tjek- og fjernfunktionen på xs
     |> rev
 printfn "               Remove Duplicates"
 printfn "Test1: %b" (removeDuplicates [1;2;1;3;2] = [1;2;3])
