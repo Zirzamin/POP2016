79df0ff3469709990b2764d722ac12e2a1c6d76e
Megahest <jonas.horstmann1@gmail.com>
2016-10-05 13:19:00 +0200

    Update, den virker men er ikke en funktion....

--- Assignment%25204/4g.22.fsx
+++ Assignment%25204/4g.22.fsx
@@ -1,10 +1,19 @@
-let rec removeDuplicates l =
-  match l with
-  |[]->[]
-  |[n]->[n]
-  |(x::x1) ->
-    let y = (removeDuplicates x1)
-    if x=y then l
-    else do (removeDuplicates x1)
-  l
-printf "%A" (removeDuplicates [1;2;1;3;1;1;4;5;2;1])
+let rec rev i =
+    match i with
+    |[] -> []
+    |[u] -> [u]
+    |p::q -> rev q @ [p]
+let rm(lst : 'a list) =
+    let f h t =
+        match t with
+        | [] -> [h]
+        | _ ->
+            if List.exists(fun x -> x = h) t = false then h :: t
+            else t
+    lst
+    |> rev
+    |> fun x -> List.foldBack f x []
+    |> rev
+printf "%A" (rm [1;4;1;1;2;2;3])
+printf "%A" (rm [1;2;1;2;3])
+printf "%A" (rm [1;2;3;4;5;5;4;3;2;1])
