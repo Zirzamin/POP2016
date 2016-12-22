badf0eb052ef0aa507cc4050f352770e914330b1
Megahest <jonas.horstmann1@gmail.com>
2016-12-22 16:08:24 +0100

    It works, dokumentation følger

--- 10g.fsx
+++ 10g.fsx
@@ -1,5 +1,4 @@
 type Animal(weight:float, maxspeed:float) =
-  let rnd = System.Random()
   let mutable crrntspeed = 0.0
   let mass = weight
   let mxspd = maxspeed
@@ -11,22 +10,36 @@ type Animal(weight:float, maxspeed:float) =
   abstract member foodneed: unit -> float
   default x.foodneed() = weight/2.0
   member x.setspeed foodintake = crrntspeed <- foodintake * maxspeed
+  member x.currentspeed = crrntspeed
 
 type Carnivore(weight:float, maxspeed:float) =
   inherit Animal(weight, maxspeed)
   override x.foodneed () =
-    x.foodneed <- (weight * 0.08)
+    (weight * 0.08)
 
 type Herbivore(weight:float, maxspeed:float) =
   inherit Animal(weight, maxspeed)
   override x.foodneed () =
-    x.foodneed <- (weight * 0.4)
+    (weight * 0.4)
 
 let cheetah = Carnivore(50.0, 114.0)
 let antelope = Herbivore(50.0, 95.0)
 let wildebeast = Herbivore(200.0, 80.0)
 let mutable foodintake = 0.0
+let rnd = System.Random()
 
 for i = 1 to 3 do
-  foodintake <- float (rnd.NextDouble())
-  printfn "The Cheetah ate %A \% of %A kg needed" (foodintake * 100.0) (cheetah.x.foodneed)
+  printfn "%A" i
+  foodintake <- rnd.NextDouble()
+  printfn "The Cheetah ate %.2f %% which is equal to %.2f kg of %.2f kg needed" (foodintake * 100.0) (foodintake * cheetah.foodneed ()) (cheetah.foodneed ())
+  cheetah.setspeed foodintake
+  printfn "The Cheetahs current speed is %.2f km/h, and it took %.2f minutes to run 10 km" (cheetah.currentspeed) ((10.0/cheetah.currentspeed)*60.0)
+  foodintake <- rnd.NextDouble()
+  printfn "The Antelope ate %.2f %% which is equal to %.2f kg of %.2f kg needed" (foodintake * 100.0) (foodintake * antelope.foodneed ()) (antelope.foodneed ())
+  antelope.setspeed foodintake
+  printfn "The Antelope current speed is %.2f km/h, and it took %.2f minutes to run 10 km" (antelope.currentspeed) ((10.0/antelope.currentspeed)*60.0)
+  foodintake <- rnd.NextDouble()
+  printfn "The Wildebeast ate %.2f %% which is equal to %.2f kg of %.2f kg needed" (foodintake * 100.0) (foodintake * wildebeast.foodneed ()) (wildebeast.foodneed ())
+  wildebeast.setspeed foodintake
+  printfn "The Wildebeast current speed is %.2f km/h, and it took %.2f minutes to run 10 km" (wildebeast.currentspeed) ((10.0/wildebeast.currentspeed)*60.0)
+  printf "\n"
